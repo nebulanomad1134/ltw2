ad8b5882f359621defcf497044d72cfaa2219b2c
ladoza03 <Dai.TQ210146@sis.hust.edu.vn>
2024-06-16 02:34:50 +0700

    add popup

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { Banner } from '../components/Banner'
-import PopularPlaces from './PopularPlaces'
+import React, { useEffect, useState } from 'react';
+import { Banner } from '../components/Banner';
+import PopularPlaces from './PopularPlaces';
+import PopupManager from '../components/Popup';
 
 const Home = () => {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/places')
+      .then((res) => res.json())
+      .then((data) => setPlaces(data));
+  }, []);
+
+  const placeIdsToShow = ['6669dcfc56b1a886315b0f1b', '666a03dfe1d1c21e8378e1d6', '666a03f1e1d1c21e8378e1da']; // Replace with your actual place IDs
+
   return (
     <div>
-      <Banner/>
-      <PopularPlaces/>
+      <Banner />
+      <PopularPlaces />
+      <PopupManager places={places} placeIds={placeIdsToShow} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
